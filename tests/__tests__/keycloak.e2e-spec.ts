7b2d97b1e799151b7cf6c0e55d26fd4bbf8a2d88
Luiz Carlos <argentinaluiz@gmail.com>
2021-03-08 06:42:23 -0300

    chore

--- tests/__tests__/keycloak.e2e-spec.ts
+++ tests/__tests__/keycloak.e2e-spec.ts
@@ -8,6 +8,7 @@ import { v4 as uuidv4 } from 'uuid';
 const chromeCapabilities = Capabilities.chrome();
 chromeCapabilities
   .set('acceptInsecureCerts', true)
+  .set('platform', 'linux')
   .set('chromeOptions', {
   'args': [
     '--disable-gpu',
